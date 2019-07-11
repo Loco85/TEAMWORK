b31725c1db2bf72597a5efbc836724e99aa45ecb
Loco85 <alexbaroyan@yandex.ru>
2019-07-11 15:05:38 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+     
+     $('.header__nav-hamburger-inner a').on('click', function() {
+     	   $('.header__nav-right').toggleClass('header__nav-right_show');
+     });
+
+});
